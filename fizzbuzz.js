fc90c6faa30a624f4682fbe11c7688cfaa5b6100
Nnenna Ndukwe <nnenna.s.ndukwe@gmail.com>
2017-10-05 09:48:23 -0400

    the official classic FizzBuzz algorithm written in JavaScript

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+var num = 1;
+
+while(num <= 100){
+	if(num % 3 === 0 && num % 5 === 0){
+		console.log("FizzBuzz");
+	}
+	else if(num % 3 === 0) {
+		console.log("Fizz");
+	}
+	else if(num % 5 === 0){
+		console.log("Buzz");
+	}
+	else{
+		console.log(num);
+	}
+	num++;
+}
