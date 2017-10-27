f07fd62dc6a09c4f16ed66c123dd8297c01a1314
Nnenna Ndukwe <nnenna.s.ndukwe@gmail.com>
2017-10-26 23:22:12 -0400

    nested API calls for Nasa facility locations and their city weather

--- NASAFacilityAndTemp/js/main.js
+++ NASAFacilityAndTemp/js/main.js
@@ -1,29 +1,44 @@
 
 
-var apiKey = "4fc6d8d8c9db36644103ba39a9d11368"
+var nasaAPI = "https://data.nasa.gov/resource/gvk9-iz74.json"
 
 $.ajax({
 
-	url:"http://api.openweathermap.org/data/2.5/weather?id=524901&APPID="+apiKey,
+	url: nasaAPI,
 	success:function(d){
 		console.log("Application loaded successfully!");
 
+
+		d.forEach(function(el){
+
+			var facilityTitle = el.facility;
+			var facilityCity = el.city;
+
+			var apiKey = "&APPID=4fc6d8d8c9db36644103ba39a9d11368";
+			var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + facilityCity + apiKey;
+
+		// second api request for async calls
 		$.ajax({
-			url:"https://data.nasa.gov/resource/gvk9-iz74.json",
-			success:function(d){
+
+			url: weatherURL,
+			success:function(r){
 				console.log("Congrats! You've successfully nested your APIs...");
+				var initialTemp = r.main.temp;
+				var conversion = (9/5) * (initialTemp-273) + 32;
+				var temperature = parseInt(conversion);
 
-
+				$("ol").append("<li>Facility Name: "+facilityTitle+", Current Temperature: "+temperature+" degrees Fahrenheit.</li>");
 			},
 			error:function(){
-				console.log("Could not connect to embedded API request...");
+				console.log("Could not connect to embedded API requests...");
 			}
 
 		});
+
+	}); 
 
 	},
 	error:function(){
 		console.log("Application broke, error somewhere...");
 	}
-
-});
+	});
